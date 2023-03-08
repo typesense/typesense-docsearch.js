ed8a02006c42c176fb5a8cab01d24f0a6cbaeca7
Jason Bosco <mail@jasonbos.co>
2023-03-08 16:35:14 -0600

    Fix build scripts

--- rollup.base.config.js
+++ rollup.base.config.js
@@ -1,13 +1,21 @@
 import json from '@rollup/plugin-json';
 import resolve from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
+import * as react from 'react';
+import * as reactDom from 'react-dom';
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
 import filesize from 'rollup-plugin-filesize';
 import { terser } from 'rollup-plugin-terser';
 
 export const plugins = [
-  commonjs(),
+  commonjs({
+    namedExports: {
+      // Source: https://github.com/rollup/rollup-plugin-commonjs/issues/290#issuecomment-537683484
+      react: Object.keys(react),
+      'react-dom': Object.keys(reactDom),
+    },
+  }),
   replace({
     __DEV__: JSON.stringify(process.env.NODE_ENV === 'development'),
   }),
