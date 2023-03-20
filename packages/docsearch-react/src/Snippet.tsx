f5d9545adfa0ed7b087d631e7072be234bbeaffd
Jason Bosco <mail@jasonbos.co>
2023-03-20 16:53:05 -0500

    Use elipsis for snippets

--- packages/docsearch-react/src/Snippet.tsx
+++ packages/docsearch-react/src/Snippet.tsx
@@ -15,12 +15,27 @@ export function Snippet<TItem extends StoredDocSearchHit>({
   tagName = 'span',
   ...rest
 }: SnippetProps<TItem>) {
+  let displayValue = hit._highlightResult[attribute]?.value || hit[attribute];
+
+  if (hit._snippetResult?.[attribute]?.value) {
+    let snippetValue = hit._snippetResult[attribute]?.value;
+    if (displayValue.substring(0, 20) !== snippetValue.substring(0, 20)) {
+      snippetValue = `... ${snippetValue}`;
+    }
+    if (
+      displayValue.substring(displayValue.length - 20, displayValue.length) !==
+      snippetValue.substring(snippetValue.length - 20, snippetValue.length)
+    ) {
+      snippetValue = `${snippetValue} ...`;
+    }
+
+    displayValue = snippetValue;
+  }
+
   return createElement(tagName, {
     ...rest,
     dangerouslySetInnerHTML: {
-      __html:
-        (hit._snippetResult ? hit._snippetResult[attribute]?.value : false) ||
-        hit[attribute],
+      __html: displayValue,
     },
   });
 }
