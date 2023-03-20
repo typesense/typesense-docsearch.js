d53305a614672a9e9273ae18234bc6fc8ecb035c
Jason Bosco <mail@jasonbos.co>
2023-03-20 18:47:43 -0500

    Working local example

--- examples/js-demo/app.js
+++ examples/js-demo/app.js
@@ -1,11 +1,29 @@
-import docsearch from '@docsearch/js/dist/umd';
+// import docsearch from '@docsearch/js/dist/umd';
+import docsearch from 'typesense-docsearch.js';
 
 import './app.css';
-import '@docsearch/css';
+
+// import '@docsearch/css';
+import 'typesense-docsearch-css';
 
 docsearch({
   container: '#docsearch',
-  indexName: 'docsearch',
-  appId: 'R2IYF7ETH7',
-  apiKey: '599cec31baffa4868cae4e79f180729b',
+  typesenseCollectionName: 'docusaurus-2', // Replace with your own doc site's name. Should match the collection name in the scraper settings.
+
+  typesenseServerConfig: {
+    nodes: [
+      {
+        host: 'x3s805zrawjuod9fp-1.a1.typesense.net',
+        port: 443,
+        protocol: 'https',
+      },
+      {
+        host: 'x3s805zrawjuod9fp-2.a1.typesense.net',
+        port: 443,
+        protocol: 'https',
+      },
+    ],
+    apiKey: 'c1DmVFTQGnnP5XtW8FV7btCDeTYhBLz6', //insert search key here
+  },
+  typesenseSearchParameters: {}
 });
