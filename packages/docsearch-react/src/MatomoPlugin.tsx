4df8dff89edffba135dcd8362d3ff0c21ce04fb0
Nico Gulden <gulden@univention.de>
2023-10-20 13:18:55 +0200

    Matomo plugin: Add conditions for sending search analytics data
    The Matomo plugin only sends data under the following conditions:
    
    * The modal UI is opened
    * The query string has length greater than 0
    * The query term wasn't used before
    
    Increase debounce limit to 500 ms.

--- packages/docsearch-react/src/MatomoPlugin.tsx
+++ packages/docsearch-react/src/MatomoPlugin.tsx
@@ -6,19 +6,22 @@ declare global {
     }
 }
 
+var query_cache = "";
+
 function _matomoSiteSearch(query: string, hits: string) {
+        query_cache = query;
         var _paq = window._paq = window._paq || [];
-        if (query.length > 0) {
-            _paq.push(['trackSiteSearch', query, false, hits]);
-        }
+        _paq.push(['trackSiteSearch', query, false, hits]);
 }
 
-var matomoSiteSearch_debounced = debounce(_matomoSiteSearch, 400);
+var matomoSiteSearch_debounced = debounce(_matomoSiteSearch, 500);
 
 export function createMatomoPlugin() {
     return {
         onStateChange({ state }) {
-            matomoSiteSearch_debounced(state.query, state.context.nbHits);
-        }
+            if ( state.isOpen && state.query.length > 0 && query_cache !== state.query ) {
+                matomoSiteSearch_debounced(state.query, state.context.nbHits);
+            }
+        },
     };
 };
